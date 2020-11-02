ddf7748244a7fed8b340656d99a40599d0a42e03
Vadim Kachevski <vadimkache@gmail.com>
2020-11-02 12:38:15 +0200

    uploading game

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 6f;
+    private SpriteRenderer SR;
+    private Rigidbody2D rb2;
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb2.AddForce(new Vector2(0,500));
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+
+                transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+                SR.flipX = true;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+      
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            SR.flipX = false;
+        }
+
+    }
+}
